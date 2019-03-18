30d4833a1398d8f951408d6960aefea22105a5b5
Karl Polintan <kf.polntan@gmail.com>
2019-03-17 21:51:03 -0700

    whatever changes i did 3/17

--- src/auth/users-model.js
+++ src/auth/users-model.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const mongoose = require ('mongoose');
+const bcrypt = require('bcrypt');
+const jsonWebToken = require('jsonwebtoken');
 
 const users = new mongoose.Schema ({
     username: {type: String, required: true, unique: true},
